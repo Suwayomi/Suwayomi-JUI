40bd70dc58b1441546e91c3b29705a7a018f0eef
Kolby Moroz Liebl <31669092+KolbyML@users.noreply.github.com>
2021-04-28 18:17:05 -0600

    Remove IE Requirement in ps1 script (#2)
    PowerShell 5 is on windows by default and uses IE by default which is no longer support on windows 10

--- scripts/SetupWindows.ps1
+++ scripts/SetupWindows.ps1
@@ -6,9 +6,9 @@ Remove-Item -Recurse -Force "tmp" -ErrorAction SilentlyContinue | Out-Null
 New-Item -ItemType Directory -Force -Path "tmp"
 
 Write-Output "Getting latest Tachidesk build files"
-$zipball = (Invoke-WebRequest -Uri "https://api.github.com/repos/Suwayomi/Tachidesk/releases/latest").content | Select-String -Pattern 'https[\.:\/A-Za-z0-9]*zipball\/[a-zA-Z0-9.]*' -CaseSensitive
+$zipball = (Invoke-WebRequest -Uri "https://api.github.com/repos/Suwayomi/Tachidesk/releases/latest" -UseBasicParsing).content | Select-String -Pattern 'https[\.:\/A-Za-z0-9]*zipball\/[a-zA-Z0-9.]*' -CaseSensitive
 
-Invoke-WebRequest -Uri $zipball.Matches.Value -OutFile tmp/Tachidesk.zip
+Invoke-WebRequest -Uri $zipball.Matches.Value -OutFile tmp/Tachidesk.zip -UseBasicParsing
 
 Expand-Archive -Path "tmp/Tachidesk.zip" -DestinationPath "tmp"
 
