5b502e9b3e14b8fc248ca9f48e52ab3eeb2664dc
Syer10 <syer10@users.noreply.github.com>
2021-08-12 23:34:44 -0400

    Update Tachidesk and fixed Server build

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -1,9 +1,9 @@
 import org.gradle.api.JavaVersion
 
 object Config {
-    const val tachideskVersion = "v0.4.3"
+    const val tachideskVersion = "v0.4.5"
     const val preview = true
-    const val previewCommit = "afabaccf1dff12936edf64e7f40070eca892fdb6"
+    const val previewCommit = "9fa17f617e497efa437f652cc7ee8139dffe2b91"
 
     val jvmTarget = JavaVersion.VERSION_15
 }
