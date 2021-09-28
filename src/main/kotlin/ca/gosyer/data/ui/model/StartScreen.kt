6d27bc36c550aa80bcf11b65f7bee6b79ec2e28d
Syer10 <syer10@users.noreply.github.com>
2021-09-28 14:09:06 -0400

    Fix copywrite headers

--- src/main/kotlin/ca/gosyer/data/ui/model/StartScreen.kt
+++ src/main/kotlin/ca/gosyer/data/ui/model/StartScreen.kt
@@ -1,9 +1,7 @@
 /*
- * Copyright (C) 2018 The Tachiyomi Open Source Project
- *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
 package ca.gosyer.data.ui.model
