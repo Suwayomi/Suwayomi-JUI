c4e13fb5cb98065824e242adeaf62164edf46f6f
Syer10 <syer10@users.noreply.github.com>
2021-10-22 14:53:44 -0400

    Update Tachidesk-Server

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -1,11 +1,11 @@
 import org.gradle.api.JavaVersion
 
 object Config {
-    const val tachideskVersion = "v0.5.3"
+    const val tachideskVersion = "v0.5.4"
     // Match this to the Tachidesk-Server commit count
-    const val serverCode = 976
+    const val serverCode = 998
     const val preview = true
-    const val previewCommit = "2cbee62f0a8fa0a431bba290e5602970687ba3b7"
+    const val previewCommit = "7c603258fbcd4571fba7ab56c6101f3cbf425a6b"
 
     val jvmTarget = JavaVersion.VERSION_15
 }
