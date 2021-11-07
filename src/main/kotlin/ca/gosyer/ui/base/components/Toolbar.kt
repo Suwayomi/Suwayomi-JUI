d7089454a1dc0ee34ea385b7ea1d0b3916813d92
Syer10 <syer10@users.noreply.github.com>
2021-11-07 14:51:56 -0500

    Make toolbar bigger and add text icon button support

--- src/main/kotlin/ca/gosyer/ui/base/components/Toolbar.kt
+++ src/main/kotlin/ca/gosyer/ui/base/components/Toolbar.kt
@@ -10,8 +10,11 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.Crossfade
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -19,14 +22,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.AppBarDefaults
 import androidx.compose.material.Card
+import androidx.compose.material.ContentAlpha
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.LocalContentColor
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -35,7 +41,9 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Close
 import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.material.icons.rounded.Sort
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -47,8 +55,11 @@ import androidx.compose.ui.input.key.KeyEventType
 import androidx.compose.ui.input.key.key
 import androidx.compose.ui.input.key.onPreviewKeyEvent
 import androidx.compose.ui.input.key.type
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -76,7 +87,7 @@ fun Toolbar(
         contentColor = contentColor
     ) {
         Row(
-            Modifier.fillMaxWidth().padding(start = 16.dp, end = 4.dp).height(56.dp),
+            Modifier.fillMaxWidth().padding(start = 16.dp, end = 4.dp).height(72.dp),
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
         ) {
@@ -108,7 +119,7 @@ fun Toolbar(
             Row(Modifier.animateContentSize()) {
                 actions()
                 if (closable) {
-                    ActionIcon(onClick = onClose, "Close", Icons.Rounded.Close)
+                    TextActionIcon(onClick = onClose, "Close", Icons.Rounded.Close)
                 }
             }
         }
@@ -164,5 +175,50 @@ fun ActionIcon(onClick: () -> Unit, contentDescription: String, icon: ImageVecto
         IconButton(onClick = onClick) {
             Icon(icon, contentDescription)
         }
+    }
+}
+
+@Composable
+fun TextActionIcon(
+    onClick: () -> Unit,
+    text: String,
+    icon: ImageVector,
+    enabled: Boolean = true,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+) {
+    Column(
+        Modifier
+            .clickable(
+                onClick = onClick,
+                enabled = enabled,
+                role = Role.Button,
+                interactionSource = interactionSource,
+                indication = rememberRipple(bounded = false, radius = 32.dp)
+            )
+            .size(56.dp),
+        verticalArrangement = Arrangement.SpaceAround,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(
+            icon,
+            text,
+            tint = if (enabled) {
+                LocalContentColor.current
+            } else {
+                LocalContentColor.current.copy(alpha = ContentAlpha.disabled)
+            }
+        )
+        Text(
+            text,
+            fontSize = 10.sp,
+            textAlign = TextAlign.Center,
+            maxLines = 1,
+            overflow = TextOverflow.Clip,
+            color = if (enabled) {
+                LocalContentColor.current
+            } else {
+                LocalContentColor.current.copy(alpha = ContentAlpha.disabled)
+            }
+        )
     }
 }
