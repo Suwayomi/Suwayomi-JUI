da197f89dc7ed2e3e7a81b2978ac498a9786e595
Syer10 <syer10@users.noreply.github.com>
2021-11-28 13:02:56 -0500

    DropdownIconButton get position better

--- src/main/kotlin/ca/gosyer/ui/base/components/DropdownIconButton.kt
+++ src/main/kotlin/ca/gosyer/ui/base/components/DropdownIconButton.kt
@@ -7,7 +7,6 @@
 package ca.gosyer.ui.base.components
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.forEachGesture
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
@@ -22,11 +21,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.awt.awtEvent
-import androidx.compose.ui.input.pointer.AwaitPointerEventScope
-import androidx.compose.ui.input.pointer.PointerEvent
-import androidx.compose.ui.input.pointer.changedToDown
-import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInWindow
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
@@ -54,26 +50,10 @@ fun DropdownIconButton(
             ) {
                 showMenu = true
             }
-            .pointerInput(Unit) {
-                forEachGesture {
-                    awaitPointerEventScope {
-                        awaitEventFirstDown().awtEvent.let {
-                            offset = DpOffset(it.xOnScreen.dp, it.yOnScreen.dp)
-                        }
-                    }
-                }
+            .onGloballyPositioned {
+                offset = it.positionInWindow().let { DpOffset(it.x.dp, it.y.dp) }
             },
         contentAlignment = Alignment.Center,
         content = content
     )
-}
-
-internal suspend fun AwaitPointerEventScope.awaitEventFirstDown(): PointerEvent {
-    var event: PointerEvent
-    do {
-        event = awaitPointerEvent()
-    } while (
-        !event.changes.all { it.changedToDown() }
-    )
-    return event
 }
