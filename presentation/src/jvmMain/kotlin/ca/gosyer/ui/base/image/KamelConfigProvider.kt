72876786d80d6b857c152f747a3f42a19ffa1ff7
Syer10 <syer10@users.noreply.github.com>
2022-03-12 21:39:11 -0500

    Fix auth and proxies for images

--- presentation/src/jvmMain/kotlin/ca/gosyer/ui/base/image/KamelConfigProvider.kt
+++ presentation/src/jvmMain/kotlin/ca/gosyer/ui/base/image/KamelConfigProvider.kt
@@ -11,20 +11,34 @@ import ca.gosyer.data.models.Manga
 import ca.gosyer.data.models.Source
 import ca.gosyer.data.server.Http
 import ca.gosyer.data.server.ServerPreferences
+import io.kamel.core.DataSource
+import io.kamel.core.Resource
 import io.kamel.core.config.DefaultCacheSize
 import io.kamel.core.config.KamelConfig
 import io.kamel.core.config.KamelConfigBuilder
+import io.kamel.core.config.ResourceConfig
 import io.kamel.core.config.fileFetcher
-import io.kamel.core.config.httpFetcher
 import io.kamel.core.config.stringMapper
 import io.kamel.core.config.uriMapper
 import io.kamel.core.config.urlMapper
+import io.kamel.core.fetcher.Fetcher
 import io.kamel.core.mapper.Mapper
 import io.kamel.image.config.imageBitmapDecoder
+import io.ktor.client.HttpClient
+import io.ktor.client.call.receive
+import io.ktor.client.features.onDownload
+import io.ktor.client.request.request
+import io.ktor.client.request.takeFrom
+import io.ktor.client.request.url
+import io.ktor.client.statement.HttpResponse
 import io.ktor.http.Url
+import io.ktor.utils.io.ByteReadChannel
 import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.channelFlow
 import me.tatarka.inject.annotations.Inject
 
 class KamelConfigProvider @Inject constructor(
@@ -46,9 +60,7 @@ class KamelConfigProvider @Inject constructor(
             fileFetcher()
 
             // JUI config
-            httpFetcher(http.engine) {
-                install(http)
-            }
+            fetcher(HttpFetcher(http))
             resourcesFetcher()
             mapper(MangaCoverMapper(serverUrl))
             mapper(ExtensionIconMapper(serverUrl))
@@ -72,5 +84,31 @@ class KamelConfigProvider @Inject constructor(
         override fun map(input: Source): Url {
             return Url(serverUrlStateFlow.value.toString() + input.iconUrl)
         }
+    }
+
+    private class HttpFetcher(private val client: HttpClient) : Fetcher<Url> {
+
+        override val source: DataSource = DataSource.Network
+
+        override val Url.isSupported: Boolean
+            get() = protocol.name == "https" || protocol.name == "http"
+
+        @OptIn(ExperimentalCoroutinesApi::class)
+        override fun fetch(
+            data: Url,
+            resourceConfig: ResourceConfig
+        ): Flow<Resource<ByteReadChannel>> = channelFlow {
+            val response = client.request<HttpResponse> {
+                onDownload { bytesSentTotal, contentLength ->
+                    val progress = (bytesSentTotal.toFloat() / contentLength).coerceAtMost(1.0F)
+                    send(Resource.Loading(progress))
+                }
+                takeFrom(resourceConfig.requestData)
+                url(data)
+            }
+            val bytes = response.receive<ByteReadChannel>()
+            send(Resource.Success(bytes))
+        }
+
     }
 }
