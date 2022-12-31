2737c9875c6e01f6b9fc37be9ef9f8701e2433a6
Syer10 <syer10@users.noreply.github.com>
2022-12-31 12:52:35 -0500

    Improve support for older versions of the server

--- domain/src/commonMain/kotlin/ca/gosyer/jui/domain/manga/model/Manga.kt
+++ domain/src/commonMain/kotlin/ca/gosyer/jui/domain/manga/model/Manga.kt
@@ -41,7 +41,7 @@ data class Manga(
     val unreadCount: Int?,
     val downloadCount: Int?,
     val chapterCount: Int?,
-    val lastChapterRead: Chapter?,
+    val lastChapterRead: Chapter? = null,
     val age: Long?,
     val chaptersAge: Long?
 )
