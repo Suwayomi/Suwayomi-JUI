a90c83b2ffac6e75c03747335a482bbcb4d63367
Syer10 <syer10@users.noreply.github.com>
2023-07-03 21:38:28 -0400

    Update Tachidesk-Server

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -6,9 +6,9 @@ object Config {
     // Tachidesk-Server version
     const val tachideskVersion = "v0.7.0"
     // Match this to the Tachidesk-Server commit count
-    const val serverCode = 1198
+    const val serverCode = 1304
     const val preview = true
-    const val previewCommit = "d4e71274f94a066309cb4881042cf4673075a5d0"
+    const val previewCommit = "9a80992aec5edfc5293f1fed79d5e34cad14cb74"
 
     val desktopJvmTarget = JavaVersion.VERSION_17
     val androidJvmTarget = JavaVersion.VERSION_11
