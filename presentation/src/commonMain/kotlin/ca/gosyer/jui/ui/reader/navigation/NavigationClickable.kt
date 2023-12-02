44aaf05ee6288d811524bc3b9d53e752b184c90a
Syer10 <syer10@users.noreply.github.com>
2023-12-01 19:21:53 -0500

    Simplify navigation clickable

--- presentation/src/commonMain/kotlin/ca/gosyer/jui/ui/reader/navigation/NavigationClickable.kt
+++ presentation/src/commonMain/kotlin/ca/gosyer/jui/ui/reader/navigation/NavigationClickable.kt
@@ -6,14 +6,11 @@
 
 package ca.gosyer.jui.ui.reader.navigation
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.awaitFirstDown
-import androidx.compose.foundation.gestures.forEachGesture
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onGloballyPositioned
@@ -21,7 +18,6 @@ import androidx.compose.ui.platform.debugInspectorInfo
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.toSize
 import ca.gosyer.jui.ui.reader.model.Navigation
-import ca.gosyer.jui.ui.util.compose.contains
 import kotlinx.coroutines.flow.MutableStateFlow
 
 fun Modifier.navigationClickable(
@@ -60,22 +56,14 @@ fun Modifier.navigationClickable(
             properties["interactionSource"] = interactionSource
         },
     ) {
-        val offsetEvent = remember { MutableStateFlow<Offset?>(null) }
         val layoutSize = remember { MutableStateFlow(Size.Zero) }
         this
-            .clickable(interactionSource, null, enabled, onClickLabel, role) {
-                val offset = offsetEvent.value ?: return@clickable
-                val size = layoutSize.value
-                if (offset in size) {
-                    onClick(navigation.getAction(offset, size))
-                }
-            }
             .pointerInput(interactionSource) {
-                forEachGesture {
-                    awaitPointerEventScope {
-                        offsetEvent.value = awaitFirstDown().position
+                detectTapGestures(
+                    onTap = {
+                        onClick(navigation.getAction(it, layoutSize.value))
                     }
-                }
+                )
             }
             .onGloballyPositioned {
                 layoutSize.value = it.size.toSize()
